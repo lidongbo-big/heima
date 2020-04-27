7f0dba00d95e1f9fa956d97f98c1e383182db704
lidongbo-big <1144086550@qq.com>
2020-04-28 07:47:25 +0800

    调整首页

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -94,18 +94,16 @@ export default {
   props: {},
   data () {
     return {
-      channels: [], // 文章频道列表
+      channels: [],
       article: {
-        title: '', // 文章标题
-        content: '', // 文章内容
-        cover: { // 文章封面
-          type: 0, // 封面类型 -1:自动，0-无图，1-1张，3-3张
-          images: [] // 封面图片的地址
+        title: '',
+        content: '',
+        cover: {
+          type: 0,
+          images: []
         },
         channel_id: null
       },
-      // 编辑器的 extensions
-      // 它们将会按照你声明的顺序被添加到菜单栏和气泡菜单中
       extensions: [
         new Doc(),
         new Text(),
@@ -113,26 +111,21 @@ export default {
         new Heading({ level: 3 }),
         new Bold({ bubble: true }), // 在气泡菜单中渲染菜单按钮
         new Image({
-          // 默认会把图片生成 base64 字符串和内容存储在一起，如果需要自定义图片上传
           uploadRequest (file) {
-            // 如果接口要求 Content-Type 是 multipart/form-data，则请求体必须使用 FormData
             const fd = new FormData()
             fd.append('image', file)
-            // 第1个 return 是返回 Promise 对象
-            // 为什么？因为 axios 本身就是返回 Promise 对象
             return uploadImage(fd).then(res => {
-              // 这个 return 是返回最后的结果
               return res.data.data.url
             })
-          } // 图片的上传方法，返回一个 Promise<url>
+          }
         }),
-        new Underline(), // 下划线
-        new Italic(), // 斜体
-        new Strike(), // 删除线
-        new HorizontalRule(), // 华丽的分割线
+        new Underline(),
+        new Italic(),
+        new Strike(),
+        new HorizontalRule(),
         new ListItem(),
-        new BulletList(), // 无序列表
-        new OrderedList(), // 有序列表
+        new BulletList(),
+        new OrderedList(),
         new TodoItem(),
         new TodoList(),
         new Fullscreen(),
@@ -146,7 +139,6 @@ export default {
           { min: 5, max: 30, message: '长度在 5 到 30 个字符', trigger: 'blur' }
         ],
         content: [
-          // { required: true, message: '请输入文章内容', trigger: 'change' }
           {
             validator (rule, value, callback) {
               console.log('content validator')
@@ -171,10 +163,6 @@ export default {
   watch: {},
   created () {
     this.loadChannels()
-
-    // 由于我们让发布和修改使用的同一个组件
-    // 所以这里要判断
-    // 如果路由路径参数中有 id，则请求展示文章内容
     if (this.$route.query.id) {
       this.loadArticle()
     }
@@ -189,51 +177,34 @@ export default {
 
     onPublish (draft = false) {
       this.$refs['publish-form'].validate(valid => {
-        // 验证失败，停止提交表单
         if (!valid) {
           return
         }
-
-        // 验证通过，提交表单
-        // 找到数据接口
-        // 封装请求方法
-        // 请求提交表单
-        // 如果是修改文章，则执行修改操作，否则执行添加操作
         const articleId = this.$route.query.id
         if (articleId) {
-          // 执行修改操作
           updateArticle(articleId, this.article, draft).then(res => {
             console.log(res)
             this.$message({
               message: `${draft ? '存入草稿' : '发布'}成功`,
               type: 'success'
             })
-            // 跳转到内容管理页面
             this.$router.push('/article')
           })
         } else {
           addArticle(this.article, draft).then(res => {
-            // 处理响应结果
-            // console.log(res)
             this.$message({
               message: `${draft ? '存入草稿' : '发布'}成功`,
               type: 'success'
             })
-            // 跳转到内容管理页面
             this.$router.push('/article')
           })
         }
       })
     },
 
-    // 修改文章：加载文章内容
     loadArticle () {
       console.log('loadArticle')
-      // 找到数据接口
-      // 封装请求方法
-      // 请求获取数据
       getArticle(this.$route.query.id).then(res => {
-        // 模板绑定展示
         this.article = res.data.data
       })
     }
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,158 +1,245 @@
 <template>
-  <div class="login-container">
-    <div class="login-form-wrap">
-      <div class="login-head">
-        <div class="logo"></div>
+  <div class="article-container">
+    <el-card class="filter-card">
+      <div slot="header" class="clearfix">
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item to="/">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>内容管理</el-breadcrumb-item>
+        </el-breadcrumb>
       </div>
-      <el-form :rules="formRules" class="login-form" ref="login-form" :model="user">
-        <el-form-item prop="mobile">
-          <el-input
-            v-model="user.mobile"
-            placeholder="请输入手机号"
-          ></el-input>
+      <el-form ref="form" :model="form" label-width="40px" size="mini">
+        <el-form-item label="状态">
+          <el-radio-group v-model="status">
+            <el-radio :label="null">全部</el-radio>
+            <el-radio :label="0">草稿</el-radio>
+            <el-radio :label="1">待审核</el-radio>
+            <el-radio :label="2">审核通过</el-radio>
+            <el-radio :label="3">审核失败</el-radio>
+            <el-radio :label="4">已删除</el-radio>
+          </el-radio-group>
         </el-form-item>
-        <el-form-item prop="code">
-          <el-input
-            v-model="user.code"
-            placeholder="请输入验证码"
-          ></el-input>
+        <el-form-item label="频道">
+          <el-select v-model="channelId" placeholder="请选择频道">
+            <el-option
+              label="全部"
+              :value="null"
+            ></el-option>
+            <el-option
+              :label="channel.name"
+              :value="channel.id"
+              v-for="(channel, index) in channels"
+              :key="index"
+            ></el-option>
+          </el-select>
         </el-form-item>
-        <el-form-item prop="agree">
-          <el-checkbox v-model="user.agree">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        <el-form-item label="日期">
+          <el-date-picker
+            v-model="rangeDate"
+            type="datetimerange"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+            :default-time="['12:00:00']"
+            format="yyyy-MM-dd"
+            value-format="yyyy-MM-dd"
+          />
         </el-form-item>
         <el-form-item>
           <el-button
-            class="login-btn"
             type="primary"
-            :loading="loginLoading"
-            @click="onLogin"
-          >登录</el-button>
+            :disabled="loading"
+            @click="loadArticles(1)"
+          >查询</el-button>
         </el-form-item>
       </el-form>
-    </div>
+    </el-card>
+
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        根据筛选条件共查询到 {{ totalCount }} 条结果：
+      </div>
+      <el-table
+        :data="articles"
+        stripe
+        style="width: 100%"
+        class="list-table"
+        size="mini"
+        v-loading="loading"
+      >
+        <el-table-column
+          prop="date"
+          label="封面">
+          <template slot-scope="scope">
+            <el-image
+              style="width: 50px; height: 50px"
+              :src="scope.row.cover.images[0]"
+              fit="cover"
+              lazy
+            >
+              <div slot="placeholder" class="image-slot">
+                加载中<span class="dot">...</span>
+              </div>
+            </el-image>
+            <!-- <img
+              v-if="scope.row.cover.images[0]"
+              class="article-cover"
+              :src="scope.row.cover.images[0]" alt=""
+            >
+            <img v-else class="article-cover" src="./no-cover.gif" alt=""> -->
+            <!--
+              下面这种情况是在运行期间动态改变处理的。
+              而本地图片必须在打包的时候就存在。
+             -->
+            <img
+              class="article-cover"
+              :src="scope.row.cover.images[0] || './no-cover.gif'" alt=""
+            >
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="title"
+          label="标题">
+        </el-table-column>
+        <el-table-column
+          label="状态">
+          <template slot-scope="scope">
+            <el-tag :type="articleStatus[scope.row.status].type">{{ articleStatus[scope.row.status].text }}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="pubdate"
+          label="发布时间">
+        </el-table-column>
+        <el-table-column
+          label="操作">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              circle
+              icon="el-icon-edit"
+              type="primary"
+              @click="$router.push('/publish?id=' + scope.row.id.toString())"
+            ></el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              icon="el-icon-delete"
+              circle
+              @click="onDeleteArticle(scope.row.id)"
+            ></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+       -->
+      <el-pagination
+        layout="prev, pager, next"
+        background
+        :total="totalCount"
+        :page-size="pageSize"
+        :disabled="loading"
+        :current-page.sync="page"
+        @current-change="onCurrentChange"
+      />
+    </el-card>
   </div>
 </template>
 
 <script>
+import {
+  getArticles,
+  getArticleChannels,
+  deleteArticle
+} from '@/api/article'
 
-import { login } from '@/api/user'
 export default {
-  name: 'LoginIndex',
+  name: 'ArticleIndex',
   components: {},
   props: {},
   data () {
     return {
-      user: {
-        mobile: '',
-        code: '',
-        agree: false
-      },
-      // checked: false,
-      loginLoading: false,
-      formRules: {
-        mobile: [
-          { required: true, message: '手机号不能为空', trigger: 'change' },
-          { pattern: /^1[3|5|7|8|9]\d{9}$/, message: '格式不正确', trigger: 'change' }
-        ],
-        code: [
-          { required: true, message: '验证码不能为空', trigger: 'change' },
-          { pattern: /^\d{6}$/, message: '格式不正确', trigger: 'change' }
-        ],
-        agree: [
-          {
-            validator: (rule, value, callback) => {
-              if (value) {
-                callback()
-              } else {
-                callback(new Error('请同意用户协议'))
-              }
-            },
-            trigger: 'change'
-          }
-        ]
-      }
+      articles: [],
+      articleStatus: [
+        { status: 0, text: '草稿', type: 'info' },
+        { status: 1, text: '待审核', type: '' },
+        { status: 2, text: '审核通过', type: 'success' },
+        { status: 3, text: '审核失败', type: 'warning' },
+        { status: 4, text: '已删除', type: 'danger' }
+      ],
+      totalCount: 0,
+      pageSize: 10,
+      status: null,
+      channels: [],
+      channelId: null,
+      rangeDate: null,
+      loading: true,
+      page: 1
     }
   },
   computed: {},
   watch: {},
-  created () {},
+  created () {
+    this.loadChannels()
+    this.loadArticles(1)
+  },
   mounted () {},
   methods: {
-    onLogin () {
-      // 获取表单数据
-      // const user = this.user
-      this.$refs['login-form'].validate(valid => {
-        if (!valid) {
-          return
-        }
-        this.login()
+    loadArticles (page = 1) {
+      this.loading = true
+      getArticles({
+        page,
+        per_page: this.pageSize,
+        status: this.status,
+        channel_id: this.channelId,
+        begin_pubdate: this.rangeDate ? this.rangeDate[0] : null,
+        end_pubdate: this.rangeDate ? this.rangeDate[1] : null
+      }).then(res => {
+        const { results, total_count: totalCount } = res.data.data
+        this.articles = results
+        this.totalCount = totalCount
+        this.loading = false
       })
     },
-    login () {
-      this.loginLoading = true
+    onCurrentChange (page) {
+      this.loadArticles(page)
+    },
 
-      login(this.user).then(res => {
-        console.log(res)
-
-        // 登录成功
+    loadChannels () {
+      getArticleChannels().then(res => {
+        this.channels = res.data.data.channels
+      })
+    },
+    onDeleteArticle (articleId) {
+      console.log(articleId)
+      console.log(articleId.toString())
+      this.$confirm('确认删除吗？', '删除提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        deleteArticle(articleId.toString()).then(res => {
+          this.loadArticles(this.page)
+        })
+      }).catch(() => {
         this.$message({
-          message: '登录成功',
-          type: 'success'
+          type: 'info',
+          message: '已取消删除'
         })
-
-        // 关闭 loading
-        this.loginLoading = false
-
-        window.localStorage.setItem('user', JSON.stringify(res.data.data))
-        // 跳转
-        this.$router.push({
-          name: 'home'
-        })
-      }).catch(err => { // 登录失败
-        console.log('登录失败', err)
-        this.$message.error('登录失败，手机号或验证码错误')
-
-        // 关闭 loading
-        this.loginLoading = false
       })
     }
-
   }
 }
 </script>
 
 <style scoped lang="less">
-.login-container {
-  position: fixed;
-  left: 0;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background: url("./login_bg.jpg") no-repeat;
+.filter-card {
+  margin-bottom: 30px;
+}
+
+.list-table {
+  margin-bottom: 20px;
+}
+
+.article-cover {
+  width: 60px;
   background-size: cover;
-  .login-form-wrap {
-    min-width: 300px;
-    padding: 40px 50px 10px;
-    background-color: #fff;
-    .login-head {
-      display: flex;
-      justify-content: center;
-      .logo {
-        width: 200px;
-        height: 57px;
-        background: url("./logo_index.png") no-repeat;
-        background-size: contain;
-      }
-    }
-    .login-form {
-      .login-btn {
-        width: 100%;
-      }
-    }
-  }
 }
 </style>
