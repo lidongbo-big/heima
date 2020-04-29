118b0a0db56f16624a93adac746333b218d10634
lidongbo-big <1144086550@qq.com>
2020-04-30 00:23:05 +0800

    个人设置

--- src/api/user.js
+++ src/api/user.js
@@ -13,3 +13,19 @@ export const getUserProfile = () => {
     url: '/mp/v1_0/user/profile'
   })
 }
+// 修改头像
+export const updateUserPhoto = data => {
+  return request({
+    method: 'PATCH',
+    url: '/mp/v1_0/user/photo',
+    data
+  })
+}
+// 修改资料
+export const updateUserProfile = data => {
+  return request({
+    method: 'PATCH',
+    url: '/mp/v1_0/user/profile',
+    data
+  })
+}
--- src/views/settings/index.vue
+++ src/views/settings/index.vue
@@ -10,7 +10,7 @@
 
         <el-row>
             <el-col :span='15'>
-              <el-form ref="form" :model="form" label-width="80px">
+              <el-form ref="form" :model="user" label-width="80px">
                 <el-form-item label="编号">
                     {{user.id}}
                 </el-form-item>
@@ -28,7 +28,7 @@
                 </el-form-item>
 
                 <el-form-item>
-                    <el-button type="primary" @click="onSubmit">保存</el-button>
+                    <el-button :loading="udploading" type="primary" @click="onSubmit">保存</el-button>
                 </el-form-item>
               </el-form>
             </el-col>
@@ -41,36 +41,33 @@
       </el-card>
         <el-dialog
         title="提示"
-        :visible.sync="dialogVisible"
+        @opened='onOpened'
+        :visible.sync='dialogVisible'
         append-to-body
+        @closed='onClosed'
         >
-        <img :src="previewImage" alt="">
+        <div class="perview-image-wrap">
+            <img class="perview-image" :src="previewImage" ref="preview-image">
+        </div>
         <span slot="footer" class="dialog-footer">
             <el-button @click="dialogVisible = false">取 消</el-button>
-            <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+            <el-button :loading="udloading" type="primary" @click="onUpdatePhoto">确 定</el-button>
         </span>
         </el-dialog>
   </div>
 </template>
 
 <script>
-import { getUserProfile } from '@/api/user'
+import 'cropperjs/dist/cropper.css'
+import Cropper from 'cropperjs'
+import { getUserProfile, updateUserPhoto, updateUserProfile } from '@/api/user'
+import globalBus from '@/utils/global-bus'
 export default {
   name: 'SettingsIndex',
   components: {},
   props: {},
   data () {
     return {
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
-      },
       user: {
         id: null,
         mobile: '',
@@ -80,7 +77,10 @@ export default {
         email: ''
       },
       dialogVisible: false,
-      previewImage: ''
+      previewImage: '',
+      cropper: null,
+      udloading: false,
+      udploading: false
     }
   },
   computed: {},
@@ -91,7 +91,15 @@ export default {
   mounted () {},
   methods: {
     onSubmit () {
-      console.log('submit!')
+      this.udploading = true
+      updateUserProfile(this.user).then(res => {
+        this.$message({
+          type: 'success',
+          message: '保存成功'
+        })
+        this.udploading = false
+        globalBus.$emit('update-user', this.user)
+      })
     },
     loadUser () {
       getUserProfile().then(res => {
@@ -104,9 +112,61 @@ export default {
       this.previewImage = blobData
       this.dialogVisible = true
       this.$refs.file.value = ''
+    },
+    onOpened () {
+      const image = this.$refs['preview-image']
+      if (this.cropper) {
+        this.cropper.replace(this.previewImage)
+        return
+      }
+      this.cropper = new Cropper(image, {
+        aspectRatio: 1,
+        viewMode: 1,
+        dragMode: 'none',
+        cropBoxResizable: false
+        // crop (event) {
+        //   console.log(event.detail.x)
+        //   console.log(event.detail.y)
+        //   console.log(event.detail.width)
+        //   console.log(event.detail.height)
+        //   console.log(event.detail.rotate)
+        //   console.log(event.detail.scaleX)
+        //   console.log(event.detail.scaleY)
+        // }
+      })
+    },
+    onClosed () {
+    //   this.cropper.destory()
+    },
+    onUpdatePhoto () {
+      this.udloading = true
+      this.cropper.getCroppedCanvas().toBlob(file => {
+        const fd = new FormData()
+        fd.append('photo', file)
+        updateUserPhoto(fd).then(res => {
+          this.dialogVisible = false
+          this.user.photo = window.URL.createObjectURL(file)
+          // this.user.photo = res.data.data.photo
+          this.udloading = false
+          this.$message({
+            type: 'success',
+            message: '更新成功'
+          })
+          globalBus.$emit('update-user', this.user)
+        })
+      })
     }
   }
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.perview-image-wrap {
+    .perview-image {
+    display: block;
+    /* This rule is very important, please don't ignore this */
+    max-width: 100%;
+    height: 200px;
+    }
+}
+</style>
